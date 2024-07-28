aff716ee084dae1fd915e7e1893c706c9a7391a4
KenAu1218 <cac2d01@gmail.com>
2024-07-29 02:24:32 +0800

    set up path alias

---
+++ src/screen/home/index.tsx
@@ -0,0 +1,31 @@
+
+import React, { Component, } from 'react'
+import {
+    AppState,
+    BackHandler,
+    Dimensions,
+    NativeModules,
+    Platform,
+    StyleSheet,
+    Text,
+    ToastAndroid,
+    TouchableWithoutFeedback,
+    View,
+} from 'react-native';
+import * as AppColors from '~/utils/colors.js';
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <View >
+                <Text>HomeScreen</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    root: {
+        backgroundColor: AppColors.primary
+    },
+});
